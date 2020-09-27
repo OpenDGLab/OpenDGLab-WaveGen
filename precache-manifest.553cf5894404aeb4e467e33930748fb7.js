c2d838d5c46e50d096bbead1399398d0f7e4ab78
sound-reload <sound-reload@users.noreply.github.com>
2020-09-27 17:18:21 +0000

    deploy: 34368a9f1307b211a84192ede66d092cc3cc440e

---
+++ precache-manifest.553cf5894404aeb4e467e33930748fb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb806a705707d4fc984543a12803dcf4",
+    "url": "/opendglab-wavegen/index.html"
+  },
+  {
+    "revision": "a985cf421d2ffd672d12",
+    "url": "/opendglab-wavegen/static/css/2.8edf6c5a.chunk.css"
+  },
+  {
+    "revision": "c930ee4f7a6f86074af0",
+    "url": "/opendglab-wavegen/static/css/main.23608bf0.chunk.css"
+  },
+  {
+    "revision": "a985cf421d2ffd672d12",
+    "url": "/opendglab-wavegen/static/js/2.d9dab9ab.chunk.js"
+  },
+  {
+    "revision": "eea2b2af4ea2927aba56086fdf001386",
+    "url": "/opendglab-wavegen/static/js/2.d9dab9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c930ee4f7a6f86074af0",
+    "url": "/opendglab-wavegen/static/js/main.f8fb8274.chunk.js"
+  },
+  {
+    "revision": "a3c2eee877feb614a4d9",
+    "url": "/opendglab-wavegen/static/js/runtime-main.358ad615.js"
+  }
+]);
